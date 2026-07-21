3eeea7d1414a727a9b96493114fe420aae04128c
adarsh-rajput01 <adarsh.25b01010177@abes.ac.in>
2026-07-21 14:50:03 +0530

    lec 1

---
+++ lab1/hello.js
@@ -0,0 +1,19 @@
+function sayHello(name){
+    console.log(`Welcome to ${name}`);
+
+}
+const sayHi = (name)=>{
+    console.log(`Welcome to ${name}`);
+};
+
+function sum(a,b){
+    return a+b;
+}
+const findSum = (a,b) => {
+    return a+b;
+}
+const findSum = (a,b) => a+b;
+
+sayHello('Adarsh')
+sayHi("Adarsh Rajput");
+console.log(`sum of 2 and 5 is ${sum(2,5)}`);
