e8f876061f3a6659418ce2044b5b70d70d4d7258
adarsh-rajput01 <adarsh.25b01010177@abes.ac.in>
2026-07-29 12:09:10 +0530

    File Delete

--- lab2/file3.js
+++ lab2/file3.js
@@ -21,4 +21,8 @@ const deleteData = async (fname) => {
   console.log(`${fname} deleted`);
 };
 
-
+await writeData("happy.txt", "I am very happy");
+await readData("happy.txt");
+await appendData("happy.txt", "FSD is interesting");
+await readData("happy.txt");
+await deleteData("happy.txt");
